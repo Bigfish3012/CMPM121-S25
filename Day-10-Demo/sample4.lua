e2b92383e5910439df45442b72a666814ab5f2a8
bigfish3012 <lichengkun0907@gmail.com>
2025-04-23 13:21:41 -0700

    update day 10 demo code

--- Day-10-Demo/sample4.lua
+++ Day-10-Demo/sample4.lua
@@ -7,8 +7,8 @@ cards = {}
 local suits = {"S", "H", "D", "C"}
 local ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
-for i=1,4 do
-    for j=1,13 do
+for i=1, #suits do
+    for j=1, #ranks do
         table.insert(cards, love.graphics.newImage("sprites/" .. suits[i] .. ranks[j] .. ".png"))
     end
 end
