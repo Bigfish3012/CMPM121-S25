98f89bb031fc606e167ea499121ccf6051a47573
Chengkun Li <bigfish@pka-100-64-115-104.resnet.ucsc.edu>
2025-04-29 18:14:23 -0700

    upload card images

--- renm.py
+++ renm.py
@@ -1,12 +1,14 @@
 import os
 
-folder = "project2/sprites/spades"
-new_names = ["SA"] + [f"S{i}" for i in range(2, 12)] + ["SJ", "SQ", "SK"]
+folders = [
+    ("project2/sprites/hearts", ["HA"] + [f"H{i}" for i in range(2, 14)])
+]
 
-files = sorted([f for f in os.listdir(folder) if f.endswith(".png")])
-for i, file in enumerate(files):
-    if i < len(new_names):
-        old = os.path.join(folder, file)
-        new = os.path.join(folder, f"{new_names[i]}.png")
-        os.rename(old, new)
-        print(f"{file} -> {new_names[i]}.png")
+for folder, new_names in folders:
+    files = sorted([f for f in os.listdir(folder) if f.endswith(".png")])
+    for i, file in enumerate(files):
+        if i < len(new_names):
+            old = os.path.join(folder, file)
+            new = os.path.join(folder, f"{new_names[i]}.png")
+            os.rename(old, new)
+            print(f"{file} -> {new_names[i]}.png in {folder}")
